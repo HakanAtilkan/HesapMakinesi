9ef4461b897c693c27d0332153d6b3aaec644575
Utku Hakan Atılkan <hakanatilkan@outlook.com>
2025-12-11 12:01:51 +0300

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace HesapMakinesi;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace HesapMakinesi;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
